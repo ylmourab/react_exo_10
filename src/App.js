ddd548aa3b242d14d94fd69dfb80c42b0553b5b5
Younes <ylmourab77@gmail.com>
2021-01-18 20:24:00 +0100

    fin exo 10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ export class App extends Component {
 
   state ={
     titre : "Bonjour",
-    paragraphe : "lorem ipsum"
+    paragraphe : "lorem ipsum",
+    titre2 : "",
+    titre3 : "",
+    paragraphe2 : ""
   }
 
   changeTitle = ()=>{
@@ -26,13 +29,44 @@ export class App extends Component {
 
     })
   }
+
+
+  saveInput = (e) =>{
+    this.setState({
+      titre2 : e.target.value,
+    })
+    console.log(e.target.value)
+  }
+
+  changeTitre = () =>{
+    this.setState({
+      titre3 : this.state.titre2,
+    })
+    
+
+  }
+
+  changeParagraphe2 = (e) =>{
+    this.setState({
+      paragraphe2 : e.target.value,
+
+    })
+  }
+
+ 
   render() {
+
+    
     return (
       <div>
         <h1 onClick={this.changeTitle}>{this.state.titre}</h1>
     <p>{this.state.paragraphe}</p>
         <button onClick={this.changeParagraphe}>modifier le paragraphe</button>
-        
+    <h1>{this.state.titre3}</h1>
+        <input onChange={this.saveInput} type="text"/>
+        <button onClick ={this.changeTitre} >clique ici</button>
+    <p>{this.state.paragraphe2}</p>
+        <textarea onChange={this.changeParagraphe2} name="" id="" cols="30" rows="10"></textarea>
       </div>
     )
   }
